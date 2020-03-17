89375e2898bc3e99d2b38a755dc142dba0596ec8
hebrewmatthew <46854918+hebrewmatthew@users.noreply.github.com>
2020-03-16 19:23:22 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ name = ‘Meter’,
 description = ‘Python-based App’,
 version = ‘1.0’,
 console=[‘mrtp.py’],
-options = {‘py2exe’: {‘bundle_files’: 1,’packages’:’ctypes’,’includes’: ‘base64,sys,socket,struct,time,code,platform,getpass,shutil’,}},
+options = {‘py2exe’: {‘bundle_files’: 1,‘packages’:‘ctypes’,‘includes’: ‘base64,sys,socket,struct,time,code,platform,getpass,shutil’,}},
 zipfile = None,
 )
